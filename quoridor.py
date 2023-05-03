32c54aaeddd76397951bce71e4c1ac80d69198c2
Ben McAuliffe <benmcauliffe@TH257G-00651LR0.pugetsound.edu>
2023-05-02 17:43:21 -0700

    added all jumping states

--- quoridor.py
+++ quoridor.py
@@ -1,6 +1,6 @@
 # Quoridor game implementation.
 #
-# Author: Julia Kaeppel
+# Author: Julia Kaeppel and Ben McAuliffe
 from enum import Enum
 import numpy as np
 
@@ -12,7 +12,8 @@ class _Direction(Enum):
 
 # The walls array has shape (2, 8, 8). The first index of the first dimension
 # is for horizontal walls, and the second is for vertical walls. The second
-# dimension is the Y dimension, and the third dimension is the X dimension.
+# dimension is the Y dimension, and the third dimension is the X dimension.s
+#(0,0) is the bottom left corner
 class Board:
     # Initializes an empty board with initial pawn placements.
     def __init__(self):
@@ -65,7 +66,7 @@ class Board:
         states = []
 
         # Place walls
-        if (p1_turn and p1_walls > 0) or (not p1_turn and p2_walls > 0):
+        if (p1_turn and self.p1_walls > 0) or (not p1_turn and self.p2_walls > 0):
             for y in range(8):
                 for x in range(8):
                     # Horizontal wall
@@ -100,7 +101,7 @@ class Board:
         
         # Pawn movement
         # Determine active and inactive pawn
-        ap, ip = self.p1, self.p2 if p1_turn else self.p2, self.p1
+        ap, ip = self.p1, self.p2 if p1_turn else self.p2, self.p1 #tuples (x,y)
 
         # Moving up
         if ap[1] < 8:
@@ -113,9 +114,135 @@ class Board:
                     else:
                         states[-1].p2 = (ap[0], ap[1] + 1)
                 else:
-                    # Handle jumping
-                    pass
-
+                    #jump handeling
+                    #Upward jump
+                    if (ap[0], ap[1] + 1) == ip and not self.wall_adj(ap[0], ap[1]+1, _Direction.UP) and not (ip[1] == 0 or ip[1] == 8):
+                        states.append(self.deepcopy())
+                        if p1_turn:
+                            states[-1].p1 = (ap[0], ap[1] + 2)
+                        else:
+                            states[-1].p2 = (ap[0], ap[1] + 2)
+                    #L jump UP and to the side
+                    if (ap[0], ap[1] + 1) == ip and self.wall_adj(ap[0], ap[1]+1, _Direction.UP) and not (ip[1] == 0 or ip[1] == 8):
+                        #Up and Right
+                        if not ip[0] == 8 and not self.wall_adj(ap[0], ap[1]+1, _Direction.RIGHT):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] + 1, ap[1] + 1)
+                            else:
+                                states[-1].p2 = (ap[0] + 1, ap[1] + 1)
+                        #Up and Left
+                        if not ip[0] == 0 and not self.wall_adj(ap[0], ap[1]+1, _Direction.LEFT):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] - 1, ap[1] + 1)
+                            else:
+                                states[-1].p2 = (ap[0] - 1, ap[1] + 1)
+        # Moving down
+        if ap[1] < 8:
+            if not self.wall_adj(ap[0], ap[1], _Direction.DOWN):
+                if (ap[0], ap[1] - 1) != ip:
+                    # Add state
+                    states.append(self.deepcopy())
+                    if p1_turn:
+                        states[-1].p1 = (ap[0], ap[1] - 1)
+                    else:
+                        states[-1].p2 = (ap[0], ap[1] - 1)
+                else:
+                    #Straight jump handeling
+                    #Downward jump
+                    if (ap[0], ap[1] + 1) == ip and not self.wall_adj(ap[0], ap[1] - 1, _Direction.DOWN) and not (ip[1] == 0 or ip[1] == 8):
+                        states.append(self.deepcopy())
+                        if p1_turn:
+                            states[-1].p1 = (ap[0], ap[1] - 2)
+                        else:
+                            states[-1].p2 = (ap[0], ap[1] - 2)
+                    #L jump Down and to the side
+                    if (ap[0], ap[1] + 1) == ip and self.wall_adj(ap[0], ap[1]+1, _Direction.Down) and not (ip[1] == 0 or ip[1] == 8):
+                        #Down and Right
+                        if not ip[0] == 8 and not self.wall_adj(ap[0], ap[1]+1, _Direction.RIGHT):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] + 1, ap[1] - 1)
+                            else:
+                                states[-1].p2 = (ap[0] + 1, ap[1] - 1)
+                        #Down and Left
+                        if not ip[0] == 0 and not self.wall_adj(ap[0], ap[1]+1, _Direction.LEFT):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] - 1, ap[1] - 1)
+                            else:
+                                states[-1].p2 = (ap[0] - 1, ap[1] - 1)
+        # Moving right
+        if ap[1] < 8:
+            if not self.wall_adj(ap[0], ap[1], _Direction.RIGHT):
+                if (ap[0] + 1, ap[1]) != ip:
+                    # Add state
+                    states.append(self.deepcopy())
+                    if p1_turn:
+                        states[-1].p1 = (ap[0] + 1, ap[1])
+                    else:
+                        states[-1].p2 = (ap[0] + 1, ap[1])
+                else:
+                    #Right jump
+                    if (ap[0] + 1, ap[1]) == ip and not self.wall_adj(ap[0] + 1, ap[1], _Direction.RIGHT) and not (ip[0] == 0 or ip[0] == 8):
+                        states.append(self.deepcopy())
+                        if p1_turn:
+                            states[-1].p1 = (ap[0] + 2, ap[1])
+                        else:
+                            states[-1].p2 = (ap[0] + 2, ap[1])
+            #L jump Right and veritically
+                    #if ip is to the right and there is a wall to the right of ip
+                    if (ap[0] + 1, ap[1]) == ip and self.wall_adj(ap[0] + 1, ap[1], _Direction.RIGHT) and not (ip[0] == 0 or ip[0] == 8):
+                        #Right and Up
+                        if not ip[1] == 8 and not self.wall_adj(ap[0] + 1, ap[1], _Direction.UP):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] + 1, ap[1] + 1)
+                            else:
+                                states[-1].p2 = (ap[0] + 1, ap[1] + 1)
+                        #Right and Down
+                        if not ip[1] == 0 and not self.wall_adj(ap[0] + 1, ap[1], _Direction.DOWN):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] + 1, ap[1] - 1)
+                            else:
+                                states[-1].p2 = (ap[0] + 1, ap[1] - 1)
+        # Moving left
+        if ap[1] < 8:
+            if not self.wall_adj(ap[0], ap[1], _Direction.LEFT):
+                if (ap[0] + 1, ap[1]) != ip:
+                    # Add state
+                    states.append(self.deepcopy())
+                    if p1_turn:
+                        states[-1].p1 = (ap[0] + 1, ap[1])
+                    else:
+                        states[-1].p2 = (ap[0] + 1, ap[1])
+                else:
+                    #Left jump
+                    if (ap[0] - 1, ap[1]) == ip and not self.wall_adj(ap[0] - 1, ap[1], _Direction.LEFT) and not (ip[0] == 0 or ip[0] == 8):
+                        states.append(self.deepcopy())
+                        if p1_turn:
+                            states[-1].p1 = (ap[0] - 2, ap[1])
+                        else:
+                            states[-1].p2 = (ap[0] - 2, ap[1])
+                    #if ip is to the left and there is a wall to the left of ip
+                    if (ap[0] + 1, ap[1]) == ip and self.wall_adj(ap[0] + 1, ap[1], _Direction.LEFT) and not (ip[0] == 0 or ip[0] == 8):
+                        #Left and Up
+                        if not ip[1] == 8 and not self.wall_adj(ap[0] + 1, ap[1], _Direction.UP):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] - 1, ap[1] + 1)
+                            else:
+                                states[-1].p2 = (ap[0] - 1, ap[1] + 1)
+                        #Left and Down
+                        if not ip[1] == 0 and not self.wall_adj(ap[0] + 1, ap[1], _Direction.DOWN):
+                            states.append(self.deepcopy())
+                            if p1_turn:
+                                states[-1].p1 = (ap[0] - 1, ap[1] - 1)
+                            else:
+                                states[-1].p2 = (ap[0] - 1, ap[1] - 1)
+            
         return states
 
     def __str__(self):
@@ -167,3 +294,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
